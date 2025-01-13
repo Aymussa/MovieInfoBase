35378e1083ab9a37091f8f35f68082cdaab9d3b1
Ahmed Mussa <109166088+Aymussa@users.noreply.github.com>
2025-01-13 10:10:39 +0000

    Update script.js with the local storage and add movie to recent

--- script.js
+++ script.js
@@ -4,13 +4,21 @@ var findMovieButton = $("#find-movie")
 function displayMovieInfo(event) {
   event.preventDefault()
   var movie = movieInput.val()
+  if (!movie.trim()) {
+    alert("Please enter a movie title!"); // Alert if input is empty
+    return;
   var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=df97ab33"
-
+    
+  // Queue to track the sequence of searched movies
+let moviesQueue = []; 
+    
+  // Fetch movie data from the API
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-
+       if (response.Response === "True") {
+      // If valid response, fetch the movie data
     console.log(response)
     $('#poster').html("<img src =' " + response.Poster + "' />");
     $("#title").text(response.Title) 
@@ -23,24 +31,51 @@ function displayMovieInfo(event) {
     $("#runTime").text(response.Runtime) 
     $("#genre").text(response.Genre) 
     $("#language").text(response.Language) 
+    
+    // Add the movie to the queue
+    moviesQueue.push(response);
+    
     //Below functions are for recent movies
-    addRecentMovie({
-      title: response.Title,
-      posterURL: response.Poster
-    });
-    renderRecentMovies();
+    
+      // Only add to the recent list after a second movie is searched
+      if (moviesQueue.length > 1) {
+        addRecentMovie(moviesQueue[0]); // Add the first movie (previously searched) to the recent list
+        renderRecentMovies(); // Re-render recent movies list
+        moviesQueue.shift(); // Remove the first movie from the queue
+      }
+    } else {
+      alert("Movie not found. Please try another movie title.");
+    }
   });
-
 }
 
+  // Local Storage Handling
+function getRecentMovies() {
+  return JSON.parse(localStorage.getItem("recentMovies")) || [];
+}
+ 
+  function addRecentMovie(movie) {
+  const recentMovies = getRecentMovies();
+  recentMovies.push(movie);
+
+  if (recentMovies.length > 5) {
+    recentMovies.shift();
+  }
+
+      localStorage.setItem("recentMovies", JSON.stringify(recentMovies));
+}
+    
 $("#find-movie").on("click", displayMovieInfo)
 
+  
 //Below code is for recent movies
 function renderRecentMovies() {
   const recentMoviesListRow = document.getElementById("recent-movies-list-row");
-
   recentMoviesListRow.innerHTML = "";
+  
   const recentMovies = getRecentMovies();
+  recentMovies.reverse();
+  
   recentMovies.forEach((movie) => {
     const movieCardHTML = `<div class="col">
       <div class="card movie-card">
@@ -61,11 +96,14 @@ window.onload = function () {
 };
 
 function clearRecentMoviesAndReloadRecentMovies() {
-  clearRecentMovies();
+  localStorage.removeItem("recentMovies");
   renderRecentMovies();
 
   var myModalEl = document.getElementById("staticBackdrop");
   var modal = bootstrap.Modal.getInstance(myModalEl);
   modal.hide();
 }
+  window.onload = function () {
+  renderRecentMovies();
+};
   
